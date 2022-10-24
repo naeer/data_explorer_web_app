00701235d04ca07b96ec6c673532ce6bb178e842
Initiator-Z <60876834+Initiator-Z@users.noreply.github.com>
2022-10-25 00:05:38 +1100

    Add files via upload

---
+++ dataframe/logics.py
@@ -0,0 +1,413 @@
+import pandas as pd
+import streamlit as st
+
+from src.database.logics import PostgresConnector
+from src.dataframe.queries import get_numeric_tables_query, get_text_tables_query, get_date_tables_query
+
+
+class Dataset:
+    """
+    --------------------
+    Description
+    --------------------
+    -> Dataset (class): Class that manages a dataset loaded from Postgres
+
+    --------------------
+    Attributes
+    --------------------
+    -> schema_name (str): Name of the dataset schema (mandatory)
+    -> table_name (str): Name of the dataset table (mandatory)
+    -> db (PostgresConnector): Instantation of PostgresConnector class for handling Postgres connection (mandatory)
+    -> df (pd.Dataframe): Pandas dataframe where the table content has been loaded (mandatory)
+    -> n_rows (int): Number of rows of dataset (optional)
+    -> n_cols (int): Number of columns of dataset (optional)
+    -> n_duplicates (int): Number of duplicated rows of dataset (optional)
+    -> n_missing (int): Number of missing values of dataset (optional)
+    -> num_cols (list): List of columns of numerical type (optional)
+    -> text_cols (list): List of columns of text type (optional)
+    -> date_cols (list): List of columns of datetime type (optional)
+    """
+    def __init__(self, schema_name=None, table_name=None, db=PostgresConnector(), df=pd.DataFrame()):
+        self.schema_name = schema_name
+        self.table_name = table_name
+        self.db = db
+        self.df = df
+        self.n_rows = None
+        self.n_cols = None
+        self.n_duplicates = None
+        self.n_missing = None
+        self.num_cols = None
+        self.text_cols = None
+        self.date_cols = None
+
+    def set_data(self):
+        """
+        --------------------
+        Description
+        --------------------
+        -> set_data (method): Class method that computes all requested information from self.df to be displayed in the Overall section of Streamlit app
+
+        --------------------
+        Parameters
+        --------------------
+        => To be filled by student
+        -> name (type): description
+
+        --------------------
+        Pseudo-Code
+        --------------------
+        => To be filled by student
+        -> pseudo-code
+
+        --------------------
+        Returns
+        --------------------
+        => To be filled by student
+        -> (type): description
+
+        """
+        self.set_numeric_columns()
+        self.set_text_columns()
+        self.set_date_columns()
+        self.is_df_none()
+        self.set_dimensions()
+        self.set_duplicates()
+        self.set_missing()
+
+    def is_df_none(self):
+        """
+        --------------------
+        Description
+        --------------------
+        -> is_df_none (method): Class method that checks if self.df is empty or none
+
+        --------------------
+        Parameters
+        --------------------
+        => To be filled by student
+        -> name (type): description
+
+        --------------------
+        Pseudo-Code
+        --------------------
+        => To be filled by student
+        -> pseudo-code
+
+        --------------------
+        Returns
+        --------------------
+        => To be filled by student
+        -> (type): description
+
+        """
+        return self.db == None | self.df.empty
+
+    def set_dimensions(self):
+        """
+        --------------------
+        Description
+        --------------------
+        -> set_dimensions (method): Class method that computes the dimensions (number of columns and rows) of self.df and store them as attributes (self.n_rows, self.n_cols)
+
+        --------------------
+        Parameters
+        --------------------
+        => To be filled by student
+        -> name (type): description
+
+        --------------------
+        Pseudo-Code
+        --------------------
+        => To be filled by student
+        -> pseudo-code
+
+        --------------------
+        Returns
+        --------------------
+        => To be filled by student
+        -> (type): description
+
+        """
+        self.n_rows = self.df.shape[0]
+        self.n_cols = self.df.shape[1]
+
+    def set_duplicates(self):
+        """
+        --------------------
+        Description
+        --------------------
+        -> set_duplicates (method): Class method that computes the number of duplicated of self.df and store it as attribute (self.n_duplicates)
+
+        --------------------
+        Parameters
+        --------------------
+        => To be filled by student
+        -> name (type): description
+
+        --------------------
+        Pseudo-Code
+        --------------------
+        => To be filled by student
+        -> pseudo-code
+
+        --------------------
+        Returns
+        --------------------
+        => To be filled by student
+        -> (type): description
+
+        """
+        self.n_duplicates = self.df.duplicated().sum()
+
+    def set_missing(self):
+        """
+        --------------------
+        Description
+        --------------------
+        -> set_missing (method): Class method that computes the number of missing values of self.df and store it as attribute (self.n_missing)
+
+        --------------------
+        Parameters
+        --------------------
+        => To be filled by student
+        -> name (type): description
+
+        --------------------
+        Pseudo-Code
+        --------------------
+        => To be filled by student
+        -> pseudo-code
+
+        --------------------
+        Returns
+        --------------------
+        => To be filled by student
+        -> (type): description
+
+        """
+        self.n_missing = self.df.isna().sum().sum()
+
+    def set_numeric_columns(self):
+        """
+        --------------------
+        Description
+        --------------------
+        -> set_numeric_columns (method): Class method that extract the list of numeric columns from a table using a SQL query (from get_numeric_tables_query()),
+        store it as attribute (self.num_cols) and then convert the relevant columns of self.df accordingly.
+
+        --------------------
+        Parameters
+        --------------------
+        => To be filled by student
+        -> name (type): description
+
+        --------------------
+        Pseudo-Code
+        --------------------
+        => To be filled by student
+        -> pseudo-code
+
+        --------------------
+        Returns
+        --------------------
+        => To be filled by student
+        -> (type): description
+
+        """
+        self.db.open_connection()
+        self.db.open_cursor()
+        self.num_cols = list(self.db.run_query(get_numeric_tables_query())[0])
+        for column in self.num_cols:
+            query = f'select {column} from {self.table_name}'
+            data = self.db.run_query(query)
+            self.df[column] = list(data[0])
+        self.db.close_cursor()
+        self.db.close_connection()
+
+    def set_text_columns(self):
+        """
+        --------------------
+        Description
+        --------------------
+        -> set_text_columns (method): Class method that extract the list of text columns from a table using a SQL query (from get_numeric_tables_query()),
+        store it as attribute (self.text_cols) and then convert the relevant columns of self.df accordingly.
+
+        --------------------
+        Parameters
+        --------------------
+        => To be filled by student
+        -> name (type): description
+
+        --------------------
+        Pseudo-Code
+        --------------------
+        => To be filled by student
+        -> pseudo-code
+
+        --------------------
+        Returns
+        --------------------
+        => To be filled by student
+        -> (type): description
+
+        """
+        self.db.open_connection()
+        self.db.open_cursor()
+        self.text_cols = list(self.db.run_query(get_text_tables_query())[0])
+        for column in self.text_cols:
+            query = f'select {column} from {self.table_name}'
+            data = self.db.run_query(query)
+            self.df[column] = list(data[0])
+        self.db.close_cursor()
+        self.db.close_connection()
+
+    def set_date_columns(self):
+        """
+        --------------------
+        Description
+        --------------------
+        -> set_date_columns (method): Class method that extract the list of datetime columns from a table using a SQL query (from get_numeric_tables_query()),
+        store it as attribute (self.date_cols) and then convert the relevant columns of self.df accordingly.
+
+        --------------------
+        Parameters
+        --------------------
+        => To be filled by student
+        -> name (type): description
+
+        --------------------
+        Pseudo-Code
+        --------------------
+        => To be filled by student
+        -> pseudo-code
+
+        --------------------
+        Returns
+        --------------------
+        => To be filled by student
+        -> (type): description
+
+        """
+        self.db.open_connection()
+        self.db.open_cursor()
+        self.date_cols = list(self.db.run_query(get_date_tables_query())[0])
+        for column in self.date_cols:
+            query = f'select {column} from {self.table_name}'
+            data = self.db.run_query(query)
+            self.df[column] = list(data[0])
+        self.db.close_cursor()
+        self.db.close_connection()
+
+    def get_head(self, n=5):
+        """
+        --------------------
+        Description
+        --------------------
+        -> get_head (method): Class method that computes the first rows of self.df according to the provided number of rows specified as parameter (default: 5)
+
+        --------------------
+        Parameters
+        --------------------
+        => To be filled by student
+        -> name (type): description
+
+        --------------------
+        Pseudo-Code
+        --------------------
+        => To be filled by student
+        -> pseudo-code
+
+        --------------------
+        Returns
+        --------------------
+        => To be filled by student
+        -> (type): description
+
+        """
+        return self.df.head(n)
+
+    def get_tail(self, n=5):
+        """
+        --------------------
+        Description
+        --------------------
+        -> get_tail (method): Class method that computes the last rows of self.df according to the provided number of rows specified as parameter (default: 5)
+
+        --------------------
+        Parameters
+        --------------------
+        => To be filled by student
+        -> name (type): description
+
+        --------------------
+        Pseudo-Code
+        --------------------
+        => To be filled by student
+        -> pseudo-code
+
+        --------------------
+        Returns
+        --------------------
+        => To be filled by student
+        -> (type): description
+
+        """
+        return self.df.tail(n)
+
+    def get_sample(self, n=5):
+        """
+        --------------------
+        Description
+        --------------------
+        -> get_sample (method): Class method that computes a random sample of rows of self.df according to the provided number of rows specified as parameter (default: 5)
+
+        --------------------
+        Parameters
+        --------------------
+        => To be filled by student
+        -> name (type): description
+
+        --------------------
+        Pseudo-Code
+        --------------------
+        => To be filled by student
+        -> pseudo-code
+
+        --------------------
+        Returns
+        --------------------
+        => To be filled by student
+        -> (type): description
+
+        """
+        return self.df.sample(n)
+
+    def get_summary_df(self):
+        """
+        --------------------
+        Description
+        --------------------
+        -> get_summary_df (method): Class method that formats all requested information from self.df to be displayed in the Overall section of Streamlit app as a Pandas dataframe with 2 columns: Description and Value
+
+        --------------------
+        Parameters
+        --------------------
+        => To be filled by student
+        -> name (type): description
+
+        --------------------
+        Pseudo-Code
+        --------------------
+        => To be filled by student
+        -> pseudo-code
+
+        --------------------
+        Returns
+        --------------------
+        => To be filled by student
+        -> (type): description
+
+        """
+        summary = pd.DataFrame()
+        summary['Description'] = ['Name of Table', 'Number of Rows', 'Number of Columns', 'Number of Duplicated Rows', 'Number of Rows with Missing Values']
+        summary['Value'] = [self.table_name, self.n_rows, self.n_cols, self.n_duplicates, self.n_missing]
---
+++ dataframe/queries.py
@@ -0,0 +1,86 @@
+def get_numeric_tables_query(schema_name, table_name):
+    """
+    --------------------
+    Description
+    --------------------
+    -> get_numeric_tables_query (method): Function that returns the query used for extracting the list of numeric columns from a Postgres table
+
+    --------------------
+    Parameters
+    --------------------
+    => To be filled by student
+    -> name (type): description
+
+    --------------------
+    Pseudo-Code
+    --------------------
+    => To be filled by student
+    -> pseudo-code
+
+    --------------------
+    Returns
+    --------------------
+    => To be filled by student
+    -> (type): description
+
+    """
+    query = f"select column_name from information_schema.columns where table_schema = '{schema_name}' and table_name = '{table_name}' and data_type in ('smallint', 'integer', 'bigint', 'decimal', 'numeric', 'real', 'double precision', 'smallserial', 'serial', 'bigserial', 'money')"
+    return query
+
+def get_text_tables_query(schema_name, table_name):
+    """
+    --------------------
+    Description
+    --------------------
+    -> get_text_tables_query (method): Function that returns the query used for extracting the list of text columns from a Postgres table
+
+    --------------------
+    Parameters
+    --------------------
+    => To be filled by student
+    -> name (type): description
+
+    --------------------
+    Pseudo-Code
+    --------------------
+    => To be filled by student
+    -> pseudo-code
+
+    --------------------
+    Returns
+    --------------------
+    => To be filled by student
+    -> (type): description
+
+    """
+    query = f"select column_name from information_schema.columns where table_schema = '{schema_name}' and table_name = '{table_name}' and data_type in ('character varying', 'character', 'text', '"char"', 'name')"
+    return query
+
+def get_date_tables_query(schema_name, table_name):
+    """
+    --------------------
+    Description
+    --------------------
+    -> get_date_tables_query (method): Function that returns the query used for extracting the list of datetime columns from a Postgres table
+
+    --------------------
+    Parameters
+    --------------------
+    => To be filled by student
+    -> name (type): description
+
+    --------------------
+    Pseudo-Code
+    --------------------
+    => To be filled by student
+    -> pseudo-code
+
+    --------------------
+    Returns
+    --------------------
+    => To be filled by student
+    -> (type): description
+
+    """
+    query = f"select column_name from information_schema.columns where table_schema = '{schema_name}' and table_name = '{table_name}' and data_type in ('timestamp without time zone', 'timestamp with time zone', 'time with time zone', 'time without time zone', 'interval', 'date')"
+    return query
